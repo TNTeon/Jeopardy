d1a91115b47c86dfeebd28de494e158a1d2cab70
ninjabelt <57401568+ninjabelt52@users.noreply.github.com>
2025-05-14 13:38:28 -0500

    stuff 'n things

--- Classes/PhoneApp/client_app.gd
+++ Classes/PhoneApp/client_app.gd
@@ -2,9 +2,16 @@ extends Control
 
 @export var startTimer : bool
 @export var timeDisplay : Label
+@export var time : float = 0
 
-var time : float = 0
 var updateTime : bool = true
+var peer = ENetMultiplayerPeer.new()
+
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
+	
+	peer.create_client("localhost",135)
+	multiplayer.multiplayer_peer = peer
 
 func _physics_process(delta: float):
 	if updateTime:
--- Tests/PhoneAppTests/multiplayer_test.gd
+++ Tests/PhoneAppTests/multiplayer_test.gd
@@ -2,21 +2,16 @@ extends Node2D
 
 var peer = ENetMultiplayerPeer.new()
 @export var player_scene: PackedScene
-
-
-func _on_host_pressed():
+func _enter_tree():
 	peer.create_server(135)
 	multiplayer.multiplayer_peer = peer
 	
 	multiplayer.peer_connected.connect(_add_player)
-	_add_player()
 	
 func _add_player(id = 1):
 	var player = player_scene.instantiate()
 	player.name = str(id)
 	call_deferred("add_child",player)
 
-
-func _on_join_pressed():
-	peer.create_client("localhost",135)
-	multiplayer.multiplayer_peer = peer
+func _physics_process(_delta):
+	print(self.get_children())
