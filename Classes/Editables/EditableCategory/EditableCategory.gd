c3cd405861652c4f730ede1d1b18bad7d13013a4
TNTeon <tnfallavol@gmail.com>
2025-05-26 22:51:35 -0500

    EditablePlayable working (no saving)
    Just need to add saving

--- Classes/Editables/EditableCategory/EditableCategory.gd
+++ Classes/Editables/EditableCategory/EditableCategory.gd
@@ -1,24 +1,33 @@
 extends Node
 class_name EditableCategory
 
+var selfResource = categoryResource.new()
+
 @export var title = ""
 @export var _questionTileScene : PackedScene
 
 @onready var storeTiles = $VBoxContainer
 @onready var _title_text : RichTextLabel = $VBoxContainer/TitlePanel/TitleText
 @onready var dead_tiles: Control = $DeadTiles
+@onready var title_panel: Panel = $VBoxContainer/TitlePanel
 
 var EditablePlayable : editablePlayable
 signal selected
 
 var numberOfTiles = 0
 func update():
-	_title_text.text = "[b]"+title
+	_title_text.text = "[b]"+title.to_upper()
+	selfResource.title = _title_text.text
+	selfResource.questionTile.clear()
+	for i in storeTiles.get_children():
+		if i is EditableTile:
+			selfResource.questionTile.append(i.selfResource)
 
 func initalize(columns : int, base : Control):
 	update()
 	EditablePlayable = base
 	selected.connect(base.newSelection)
+	base.removeSelections.connect(clearSelection)
 	for i in range(columns):
 		newTile()
 
@@ -33,6 +42,7 @@ func newTile():
 		var child = dead_tiles.get_child(dead_tiles.get_child_count()-1)
 		child.reparent(storeTiles)
 		child.visible = true
+	update()
 	
 func removeTile():
 	var child = storeTiles.get_child(storeTiles.get_child_count()-1)
@@ -41,6 +51,7 @@ func removeTile():
 	if EditablePlayable.currentSelection == child:
 		selected.emit(null)
 	numberOfTiles -= 1
+	update()
 	
 func updateColumns(columns : int):
 	while columns > numberOfTiles:
@@ -49,7 +60,19 @@ func updateColumns(columns : int):
 		removeTile()
 		
 func checkIfSelected():
+	if EditablePlayable.currentSelection == self:
+		selected.emit(null)
+		return
 	for i in storeTiles.get_children():
 		if EditablePlayable.currentSelection == i:
 			selected.emit(null)
 			break
+
+func _on_button_pressed() -> void:
+	selected.emit(self)
+	var new_stylebox_normal = title_panel.get_theme_stylebox("panel").duplicate()
+	new_stylebox_normal.border_color = Color.DARK_BLUE
+	title_panel.add_theme_stylebox_override("panel", new_stylebox_normal)
+	
+func clearSelection():
+	title_panel.remove_theme_stylebox_override("panel")
--- Classes/Editables/EdtiablePlayable/editable_playable.gd
+++ Classes/Editables/EdtiablePlayable/editable_playable.gd
@@ -13,12 +13,16 @@ var currentSelection = null
 @onready var quInput : TextEdit = $HBoxContainer/VBoxContainer/TileEdit/quInput
 @onready var anInput : TextEdit = $HBoxContainer/VBoxContainer/TileEdit/anInput
 
+@onready var category_edit: VBoxContainer = $HBoxContainer/VBoxContainer/CategoryEdit
+@onready var title_input: TextEdit = $HBoxContainer/VBoxContainer/CategoryEdit/titleInput
+
 var autoChanging = false
 
 signal removeSelections
 
 func _ready():
 	tile_edit.visible = false
+	category_edit.visible = false
 	_on_rows_int_select_value_changed(5)
 	_on_columns_int_select_value_changed(5)
 
@@ -35,14 +39,16 @@ func _on_columns_int_select_value_changed(value: float) -> void:
 	columns = int(value)
 	editable_board.updateColumns(columns)
 
-func newSelection(node : EditableTile):
+func newSelection(node):
 	removeSelections.emit()
 	currentSelection = node
-	if node:
+	if node is EditableTile:
 		var tileResouce = node.selfResource
 		tile_edit.visible = true
+		category_edit.visible = false
 		
-		autoChanging = true
+		if pvInput.value != tileResouce.pointValue:
+			autoChanging = true
 		pvInput.value = tileResouce.pointValue
 		if tileResouce.Question != "":
 			quInput.text = tileResouce.Question
@@ -52,11 +58,28 @@ func newSelection(node : EditableTile):
 			anInput.text = tileResouce.Answer
 		else:
 			anInput.text = ""
+	elif node is EditableCategory:
+		var catResouce = node.selfResource
+		tile_edit.visible = false
+		category_edit.visible = true
+		if catResouce.title != "No Title":
+			title_input.text = catResouce.title.replace("[b]","")
+		else:
+			title_input.text = ""
+		
 	else:
 		tile_edit.visible = false
-		
+		category_edit.visible = false
+	
 func tile_input_changed(test = null) -> void:
 	if currentSelection is EditableTile and not autoChanging:
 		currentSelection.update(int(pvInput.value), quInput.text.to_upper(), anInput.text.to_upper())
+	if autoChanging:
+		autoChanging = false
+		
+func cat_input_changed() -> void:
+	if currentSelection is EditableCategory and not autoChanging:
+		currentSelection.title = title_input.text
+		currentSelection.update()
 	if autoChanging:
 		autoChanging = false
