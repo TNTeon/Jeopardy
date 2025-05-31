bab4738d4315a58432c851c7074f5703cbd91716
TNTeon <39466405+TNTeon@users.noreply.github.com>
2025-05-30 20:30:28 -0500

    Added scaling text and I think its working?!

--- Classes/LittleAdditions/ScaleableText/scaleable_textl.gd
+++ Classes/LittleAdditions/ScaleableText/scaleable_textl.gd
@@ -3,28 +3,31 @@ extends RichTextLabel
 class_name scaleableText
 
 @export var fontType : String = "normal_font"
+@export var extraSpace : float = 1
 
 func _on_resized():
 	resizeFont()
 
 func resizeFont():
 	var lengthOfFont = getFontLength(get_theme_font_size(fontType+"_size"),"x")
-	var lengthOfWindow = (size.x-position.x)*scale.x
+	var lengthOfWindow = (size.x)*scale.x * extraSpace
 	var lengthScaleAmount = lengthOfWindow/lengthOfFont
 	
 	var heightOfFont = getFontLength(get_theme_font_size(fontType+"_size"),"y")
-	var heightOfWindow = (size.y-position.y)*scale.y
+	var heightOfWindow = (size.y)*scale.y * extraSpace
 	var heightScaleAmount = heightOfWindow/heightOfFont
 	
 	var smallerMultiplier = min(heightScaleAmount,lengthScaleAmount)
 	add_theme_font_size_override(fontType+"_size",get_theme_font_size(fontType+"_size") * smallerMultiplier)
 	
 	lengthOfFont = getFontLength(get_theme_font_size(fontType+"_size"),"x")
-	lengthOfWindow = (size.x-position.x)*scale.x
+	lengthOfWindow = (size.x)*scale.x - extraSpace
 	heightOfFont = getFontLength(get_theme_font_size(fontType+"_size"),"y")
-	heightOfWindow = (size.y-position.y)*scale.y
+	heightOfWindow = (size.y)*scale.y - extraSpace
 	if lengthOfFont > lengthOfWindow or heightOfFont > heightOfWindow:
-		add_theme_font_size_override(fontType+"_size",get_theme_font_size(fontType+"_size")-10)
+		print("length: ",lengthOfWindow - lengthOfFont, "height: ",heightOfWindow - heightOfFont)
+		add_theme_font_size_override(fontType+"_size",get_theme_font_size(fontType+"_size")-20)
+	print("length: ",lengthOfWindow - lengthOfFont, "height: ",heightOfWindow - heightOfFont)
 	
 
 func getFontLength(fontSize, axis : String):
